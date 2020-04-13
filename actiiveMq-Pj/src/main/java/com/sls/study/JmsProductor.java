e4e53c2b547900a60192bf7a6d1c92d335cddf75
96226 <962261337@qq.com>
2020-04-13 23:59:12 +0800

    jms 简单的生产者和消费者示例 消费者消费有一个阻塞接收和监听接收

---
+++ actiiveMq-Pj/src/main/java/com/sls/study/JmsProductor.java
@@ -0,0 +1,46 @@
+package com.sls.study;
+
+import org.apache.activemq.ActiveMQConnectionFactory;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.jms.*;
+
+public class JmsProductor {
+
+    private  static  final Logger logger = LoggerFactory.getLogger(JmsProductor.class);
+
+    private static final String ACTIVE_CONN = "tcp://192.168.1.11:61616";
+    private static final String QUEUE_NAME = "tcp://localhost:61616";
+    public static void main(String[] args) throws JMSException {
+        // 根据连接工厂创建
+        ActiveMQConnectionFactory connectionFactory = new ActiveMQConnectionFactory(ACTIVE_CONN);
+
+        //获取连接
+        Connection connection = connectionFactory.createConnection();
+        // 开启连接
+        connection.start();
+
+        //获取会话2  事务/签收
+        Session session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
+
+        // 创建目的地
+        Queue queue = session.createQueue(QUEUE_NAME);
+        // 创建生产者
+        MessageProducer producer = session.createProducer(queue);
+
+        for (int i = 1; i <= 3; i++) {
+            //创建消息
+            TextMessage textMessage = session.createTextMessage();
+
+            textMessage.setText("***生产者生产"+i);
+
+            producer.send(textMessage);
+        }
+        producer.close();
+        session.close();
+        connection.close();
+        logger.info("生产者结束了" + Thread.currentThread().getName());
+
+    }
+}
